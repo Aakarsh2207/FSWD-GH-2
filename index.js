7ce1de84178d1ae1301c19f4f32907b4e775933b
Aakarsh2207 <aakarsh22sharma007@gmail.com>
2024-06-04 18:32:28 +0530

    Day5 done

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ const htmlTaskContent = ({id, title, description, type, url}) =>
 `<div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
         <div class="card shadow-sm task__card">
             <div class="card-header d-flex justify-content-end task__card__header gap-2">
-                <button type="button" class="btn btn-outline-primary mr-2" name=${id}>     <!-- Edit Button1 -->     <!-- Primary is BLUE -->
+            <button type="button" class="btn btn-outline-primary mr-2" name=${id} onclick="editTask.apply(this, arguments)">
                     <i class="fa-solid fa-pencil" name=${id}></i>
                 </button>
-                 <button type="button" class="btn btn-outline-danger mr-2" name=${id} onclick="deleteTask.apply(this, arguments)">     <!-- Edit Button2 -->     <!-- Danger is RED --><!-- "openTask" alone will not work, We have to write this line full JS130 -->
+                 <button type="button" class="btn btn-outline-danger mr-2" name=${id} onclick="deleteTask.apply(this, arguments)">     
                     <i class="fa-solid fa-trash" name=${id}></i>
                 </button>
-            </div>     <!-- This HTML code card is Dynamic right now, Copy same in HTML in last of Body Tag to view this HTML code Function in Static Mode -->
-            <div class="card-body">     <!-- Check if user entered the URL or not -->
+            </div>
+            <div class="card-body">
                 ${
                     url ? `<img src=${url} alt='card image top' class="card-img-top md-3 rounded"/>` :
                         `<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScujirQqIFjN5GuM1565_-DIX6OyU_96HzNBl_BAX8GL0JzMs8&s" alt='card image top' class="card-img-top md-3 rounded"/>`
@@ -57,12 +57,12 @@ const htmlTaskContent = ({id, title, description, type, url}) =>
                 </div>
             </div>
             <div class="card-footer">
-                <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask" id=${id} onclick="openTask.apply(this, arguments)">Open Task</button>     <!-- On Click, GOTO openTask...   (JS121) -->     <!-- "openTask" alone will not work, We have to write this line full (JS120) -->
+                <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask" id=${id} onclick="openTask.apply(this, arguments)">Open Task</button>
             </div>
         </div>
     </div>
 `;
-
+//We cannot write HTML comments in JS so....        //JS41=     <!-- Edit Button1 -->     <!-- Primary is BLUE -->     <!-- "editTask" alone will not work, We have to write this line full JS172 -->       //JS44=     <!-- Edit Button2 -->     <!-- Danger is RED -->     <!-- "deleteTask" alone will not work, We have to write this line full JS130 -->           //JS47=          <!-- This HTML code card is Dynamic right now, Copy same in HTML in last of Body Tag to view this HTML code Function in Static Mode -->               //JS48=           <!-- Check if user entered the URL or not -->              //JS60=          <!-- On Click, GOTO openTask...   (JS121) -->     <!-- "openTask" alone will not work, We have to write this line full (JS120) -->
 
 const htmlModalContent = ({id, title, description, url}) => {
     const date = new Date(parseInt(id));
@@ -166,4 +166,87 @@ const searchTask = (e) => {     //e=event       //HTML212
        resultData.map((cardData) => {
          taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardData))
        })
+}
+   
+
+const editTask = (e) => {
+    if(!e) e = window.event;
+
+    const targetId = e.target.id;
+    const type = e.target.tagName;
+    
+    let parentNode;
+    let taskTitle;
+    let taskDescription;
+    let taskType;
+    let submitButton;
+
+    if(type === "BUTTON"){
+     parentNode = e.target.parentNode.parentNode;
+    }else{
+      parentNode = e.target.parentNode.parentNode.parentNode;
+    }
+
+    taskTitle = parentNode.childNodes[3].childNodes[3];
+ //    console.log(taskTitle);
+    taskDescription = parentNode.childNodes[3].childNodes[5];
+ //    console.log(taskDescription)
+    taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+ //    console.log(taskType);
+ submitButton = parentNode.childNodes[5].childNodes[1];
+
+ taskTitle.setAttribute ("contenteditable", "true");
+ taskDescription.setAttribute ("contenteditable", "true");
+ taskType.setAttribute ("contenteditable", "true");
+
+ submitButton.setAttribute('onclick', "saveEdit.apply(this, arguments)");
+ submitButton.removeAttribute("data-bs-toggle");
+ submitButton.removeAttribute("data-bs-target");
+ submitButton.innerHTML = "Save Changes"
+}   
+
+
+const saveEdit = (e) => {
+    if(!e) e = window.event;
+
+    const targetId = e.target.id;
+    const parentNode = e.target.parentNode.parentNode;
+ //    console.log(parentNode.childNodes)
+
+ const taskTitle = parentNode.childNodes[3].childNodes[3];
+ const taskDescription = parentNode.childNodes[3].childNodes[5];
+ const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+ const submitButton = parentNode.childNodes[5].childNodes[1];
+
+ const updateData = {       //Update the contents
+     taskTitle : taskTitle.innerHTML,
+     taskDescription: taskDescription.innerHTML,
+     taskType: taskType.innerHTML
+ }
+
+ let stateCopy = state.taskList;
+
+ stateCopy = stateCopy.map((task)=>
+     task.id === targetId ? {
+         ...task,       //Image was not displaying when "Open Task" without this
+         id: task.id,
+         title: updateData.taskTitle,
+         description: updateData.taskDescription,
+         type: updateData.taskType,
+         
+     } : task
+ );
+
+ state.taskList = stateCopy;
+ updateLocalStorage();
+
+ taskTitle.setAttribute ("contenteditable", "false");
+ taskDescription.setAttribute ("contenteditable", "false");
+ taskType.setAttribute ("contenteditable", "false");
+
+ 
+ submitButton.setAttribute('onclick', "openTask.apply(this, arguments)");       //Same 4lines from JS202 with few changes
+ submitButton.setAttribute("data-bs-toggle", "modal");
+ submitButton.setAttribute("data-bs-target", "#showTask");
+ submitButton.innerHTML = "Open Task"       //REVERSE OF - #...Button changes to this from Open Task
 }
